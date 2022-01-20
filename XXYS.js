6f1db88448cdb4bdbdbb51e901de836ed43b529a
cefv12 <82319688+cefv12@users.noreply.github.com>
2022-01-20 22:16:27 +0800

    XXYS.js
    111

---
+++ XXYS.js
@@ -0,0 +1,10 @@
+var obj = JSON.parse($response.body); 
+// 可以合并一句带过
+obj=
+{"retcode":0,"errmsg":"","data":{"user":{"uid":"10357243","uniqkey":"LL1JOV","username":"~小小影迷-LL1JOV","nickname":"","mobi":"86.1","email":"~1305133807","sysgid":"5","gid":"1","gids":null,"gicon":"V5","isvip":1,"regtime":"2022-01-19 13:43:38","gender":"0","avatar":"","avatar_url":"https://img.yuanmifang.com/sysavatar/noavatar.png","newmsg":"1","goldcoin":1,"duetime":"2999-01-22 13:43:38","dueday":"999天后过期"},"uinfo":{"goldcoin":"1","play_daily_remainders":999,"down_daily_remainders":100,"curr_group":{"gid":"5","gname":"尊贵VIP","gicon":"V5","minup":"20"},"next_group":{"gid":"6","gname":"禁止发言","gicon":"","minup":"65535"},"next_upgrade_need":65535},"signed":0,"reqplay_addnum":"0","reqdown_addnum":"0","groups":[{"gname":"游客","gicon":"V0","minup":"0","play_daynum":15,"down_daynum":0,"comment_daynum":0},{"gname":"VIP1","gicon":"V1","minup":"0","play_daynum":25,"down_daynum":4,"comment_daynum":5},{"gname":"VIP2","gicon":"V2","minup":"2","play_daynum":50,"down_daynum":10,"comment_daynum":10},{"gname":"VIP3","gicon":"V3","minup":"5","play_daynum":100,"down_daynum":20,"comment_daynum":15},{"gname":"VIP4","gicon":"V4","minup":"10","play_daynum":200,"down_daynum":40,"comment_daynum":20},{"gname":"尊贵VIP","gicon":"V5","minup":"20","play_daynum":999,"down_daynum":100,"comment_daynum":30}]}};
+
+
+// 有需要全部替换的情况，只需让用户在这里如此操作即可
+
+$done({body:JSON.stringify(obj)});
+// 也一句带过
