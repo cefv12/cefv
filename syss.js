887e8bd02881de76cf6ec1352c69495a96ad6f2d
cefv12 <82319688+cefv12@users.noreply.github.com>
2022-02-19 00:31:57 +0800

    Update syss.js

--- syss.js
+++ syss.js
@@ -1,10 +1,20 @@
 var obj = JSON.parse($response.body); 
 // 可以合并一句带过
 obj = 
+
 {
   "errno": 0,
   "errmsg": "",
-  "data": ""
+  "data": {
+    "title": "",
+    "cover": "",
+    "video": "",
+    "mp4Url": ""
+    "type": 1,
+    "me": 1,
+    "minCoverUrl": "",
+    "surplusCount": 4264
+  }
 }
 
 // 有需要全部替换的情况，只需让用户在这里如此操作即可
