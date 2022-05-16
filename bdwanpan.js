504b360c103b59016944252d19db6b60fd81cacb
cefv12 <82319688+cefv12@users.noreply.github.com>
2022-05-17 00:19:34 +0800

    Update bdwanpan.js

--- bdwanpan.js
+++ bdwanpan.js
@@ -4,7 +4,7 @@ Surge4.0:
 http-response https:\/\/pan\.baidu\.com\/rest\/2\.0\/membership\/user requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/zwf234/rules/master/js/BaiduCloud.js
 
 QX1.0.0:
-https:\/\/pan\.baidu\.com\/rest\/2\.0\/membership\/user url script-response-body https://raw.githubusercontent.com/cefv12/cefv/bdwanpan.js
+https:\/\/pan\.baidu\.com\/rest\/2\.0\/membership\/user url script-response-body https://raw.githubusercontent.com/cefv12/cefv/111/bdwanpan.js
 
 MITM = pan.baidu.com
 */
