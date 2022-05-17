02763b87d59c22411a7984e639dabe6477a0ee9a
cefv12 <82319688+cefv12@users.noreply.github.com>
2022-05-17 09:31:08 +0800

    Update bdwanpan1.js

--- bdwanpan1.js
+++ bdwanpan1.js
@@ -11,7 +11,7 @@ hostname = pan.baidu.com
 
 let obj = JSON.parse($response.body);
 obj = {
-    "product_infos": [{
+  "product_infos": [{
     "product_id": "",
     "start_time": 1650815999,
     "end_time": 3653407999,
@@ -39,10 +39,10 @@ obj = {
     "buy_description": "",
     "product_description": ""
   }],
-  "currenttime": 1652746284,
+  "currenttime": 1652749167,
   "reminder": {
     "svip": {
-      "leftseconds": 661715,
+      "leftseconds": 658833,
       "nextState": "normal"
     },
     "reminderWithContent": [],
@@ -55,9 +55,9 @@ obj = {
     "product_id": "12187135090581539740"
   },
   "level_info": {
-    "current_value": 1660,
+    "current_value": 2400,
     "current_level": 2,
-    "history_value": 1660,
+    "history_value": 2400,
     "history_level": 2,
     "last_manual_collection_time": 0
   },
@@ -74,6 +74,6 @@ obj = {
   "current_privilege_card_v2": {},
   "last_privilege_card_v2": {},
   "user_tag": "{\"has_buy_record\":1,\"has_buy_vip_svip_record\":1,\"last_buy_record_creat_time\":1650798013,\"is_vip\":0,\"is_svip\":1,\"last_vip_type\":1,\"last_vip_svip_end_time\":3653407999,\"is_svip_sign\":1,\"notice_user_type\":0,\"notice_user_status\":3}",
-  "request_id": 8926746124801195556
+  "request_id": 8927519924141496729
 };
 $done({body: JSON.stringify(obj)});
