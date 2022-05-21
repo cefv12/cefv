f9a86d073f423cdd183afb4059d866174d5b62a0
cefv12 <82319688+cefv12@users.noreply.github.com>
2022-05-21 16:09:09 +0800

    Update yqbz.js

--- yqbz.js
+++ yqbz.js
@@ -1,53 +1,59 @@
 /*
 解锁vip
-https:\/\/wpvip-ios\.duba\.net\/api\/sdk\/user/userInfo url script-response-body https://raw.githubusercontent.com/cefv12/cefv/111/yqbz.js
+https:\/\/mobile-ios-wallpaper-api\.zhhainiao.com\/v1\/user\/info url script-response-body https://raw.githubusercontent.com/cefv12/cefv/111/yqbz.js
 [mitm]
-hostname = wpvip-ios.duba.net
+hostname = mobile-ios-wallpaper-api.zhhainiao.com
 */
 
 let obj = JSON.parse($response.body);
 obj = {
-  "data" : {
-    "user_info" : {
-      "education" : 0,
-      "birthday" : "",
-      "current_time" : 1653119025,
-      "vip_type" : "1",
-      "nickname" : "188******91",
-      "register_frm" : 1,
-      "last_vip_expire_time" : 1753119025,
-      "login_frm" : 1,
-      "is_continuous" : "1",
-      "bind_info" : {
-        "open_id" : "",
-        "token" : "",
-        "has_server_id" : true
-      },
-      "latest_order_type" : "1753119025",
-      "sign" : "",
-      "gender" : 0,
-      "profession" : 1,
-      "email" : "",
-      "vip_ex_date" : 1953119025,
-      "mobile" : "1753119025",
-      "continuous_code" : "1",
-      "latest_order_id" : "",
-      "open_id" : "1527179947654909952",
-      "avatar" : "http://newvip-cdn.zhhainiao.com/public/avatar/avatar2@2x.jpg",
-      "vip_sub_ex_date" : 1753119025,
-      "vip_sub_type" : "1",
-      "address" : "",
-      "is_tourist" : true
-    },
-    "permission_info" : 1753119025,
-    "permission_code" : "1753119025",
-    "permission_list" : "1753119025",
-    "permission_code_status" : 1
+  "user_info" : {
+    "mobile_like_static_cnt" : 1,
+    "works_mobile_live_cnt" : 1,
+    "following_count" : 1,
+    "vip_type" : 1,
+    "nickname" : "188******91",
+    "wx_openid" : "",
+    "works_mobile_download_cnt" : 1,
+    "works_mobile_like_cnt" : 1,
+    "mobile_like_cnt" : 1,
+    "user_desc" : "",
+    "like_wp_cnt" : 1,
+    "buy_wp_cnt" : 1,
+    "mobile_static_buy_wp_cnt" : 1,
+    "follower_count" : 1,
+    "works_scan_cnt" : 1,
+    "like_static_wp_cnt" : 1,
+    "mobile_buy_wp_cnt" : 1,
+    "static_buy_wp_cnt" : 1,
+    "works_mobile_cnt" : 1,
+    "uid" : 39980759,
+    "works_mobile_static_cnt" : 1,
+    "user_type" : 1,
+    "is_real_name_auth" : 1,
+    "works_live_cnt" : 1,
+    "works_mobile_scan_cnt" : 1,
+    "avatar" : "http://newvip-cdn.zhhainiao.com/public/avatar/avatar2@2x.jpg",
+    "works_like_cnt" : 1,
+    "works_static_cnt" : 1,
+    "contract_status" : 1,
+    "contract_user_type" : 1,
+    "works_cnt" : 1,
+    "works_download_cnt" : 1
   },
+  "platforms" : [
+    {
+      "last_time" : 1653118358,
+      "first_time" : 1753119025,
+      "refer" : "",
+      "frm" : "",
+      "platform" : "ios"
+    }
+  ],
   "resp_common" : {
     "ret" : 1,
     "msg" : "ok",
-    "request_id" : "62889831-a9fcd04"
+    "request_id" : "62889c48-78773e6e"
   }
 };
 
