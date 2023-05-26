962162ab6d13d77ac25324f26e39ca17ef86659c
cefv12 <82319688+cefv12@users.noreply.github.com>
2023-05-26 13:04:27 +0800

    mkydq3.js

---
+++ mkydq3.js
@@ -0,0 +1,26 @@
+/*
+
+漫客阅读
+
+https:\/\/*.klzdp.com\/app\/308\/e3\/f3\/*_reload.html  url script-response-body  https://raw.githubusercontent.com/cefv12/cefv/111/mkydq3.js
+
+[mitm]
+
+ 
+
+hostname = *.klzdp.com
+
+*/
+
+let obj = JSON.parse($response.body);
+
+obj = 
+{
+  "data" : {
+    "ver" : 1,
+    "content" : "RDNHNkJRRjlWRDg5N0JRVsgOjnsD7iP564rtuMN3Xtx0P1XSY9iOGJxUMiHS6g5oTjdHSlUyVkJZV0pOVlFDRA=="
+  },
+  "code" : 1
+};
+
+$done({body: JSON.stringify(obj)});
