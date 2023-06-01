3f6424b0ac7d4e0d6fbc48011ebaf62872b47644
cefv12 <82319688+cefv12@users.noreply.github.com>
2023-06-01 23:25:43 +0800

    Create fyb.js

---
+++ fyb.js
@@ -0,0 +1,17 @@
+/*
+飞韵听书
+https:\/\/fyts-cloud2.gz.bcebos.com\/cloud\/app\/com.m.lis\/appTrans\/* url script-response-body  https://raw.githubusercontent.com/cefv12/cefv/111/fyb.js
+
+[mitm]
+
+hostname = fyts-cloud2.gz.bcebos.com
+*/
+let obj = JSON.parse($response.body);
+
+obj = 
+{
+  "message" : "YQnb1i4ZMvInyPVs5fl4AZhHAGlXUghz",
+  "requestId" : "a1022b34-39f6-4a4c-8df9-877a32765821",
+  "code" : "200"
+};
+$done({body: JSON.stringify(obj)});
