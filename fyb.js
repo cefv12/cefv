75463c91a037186fee8591d8b66d08f2f27e42c2
cefv12 <82319688+cefv12@users.noreply.github.com>
2023-06-01 23:36:45 +0800

    Update fyb.js

--- fyb.js
+++ fyb.js
@@ -1,6 +1,6 @@
 /*
 飞韵听书
-https:\/\/fyts-cloud2.gz.bcebos.com\/cloud\/app\/com.m.lis\/ios\/* url script-response-body  https://raw.githubusercontent.com/cefv12/cefv/111/fyb.js
+https:\/\/fyts-cloud2.gz.bcebos.com\/cloud\/app\/com.m.lis\/ios|serverinfo*\/* url script-response-body  https://raw.githubusercontent.com/cefv12/cefv/111/fyb.js
 
 [mitm]
 
@@ -10,8 +10,22 @@ let obj = JSON.parse($response.body);
 
 obj = 
 {
-  "message" : "YQnb1i4ZMvInyPVs5fl4AZhHAGlXUghz",
-  "requestId" : "a1022b34-39f6-4a4c-8df9-877a32765821",
-  "code" : "200"
+  "message" : "SUCCESS",
+  "imagehost" : "https://imgapixs.pysmei.com",
+    "souhost" : "",
+    "showaddays" : 0,
+    "new_rep_host" : "",
+    "yyconf" : [
+      {
+        "yyappid" : "24414049",
+        "yyapikey" : "YTddecEbXNFsFiL4wn1GinDS",
+        "yyskey" : "YQnb1i4ZMvInyPVs5fl4AZhHAGlXUghz"
+      }
+    ],
+    "dirhost" : "https://infosxs.pysmei.com",
+    "preload_time" : 0,
+    "xzhost" : ""
+  },
+  "code" : 0
 };
 $done({body: JSON.stringify(obj)});
